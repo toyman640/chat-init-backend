e875a859c100ba056922db27b0b57ebc84de2ae0
toyman640 <falakoadebayor@gmail.com>
2024-01-28 12:30:51 +0100

    Set up method to handle login

--- chat_backend/chat_app/api/urls.py
+++ chat_backend/chat_app/api/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, CreateUserView
+from .views import UserViewSet, CreateUserView, LoginView
 
 user_router = DefaultRouter()
 user_router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    # Include the automatically generated URL patterns for UserViewSet
     path('api/', include(user_router.urls)),
-    
-    # Manually define a path for CreateUserView
     path('api/create-user/', CreateUserView.as_view(), name='create_user'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- chat_backend/chat_app/api/views.py
+++ chat_backend/chat_app/api/views.py
@@ -1,10 +1,20 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 from rest_framework.viewsets import ModelViewSet
 from ..models import CustomUser
 from .serializers import UserSerializer
-# from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
+
+class CustomObtainAuthToken(ObtainAuthToken):
+  def post(self, request, *args, **kwargs):
+    serializer = self.serializer_class(data=request.data, context={'request': request})
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key, 'user_id': user.pk, 'username': user.username})
 
 class UserViewSet(ModelViewSet):
   queryset = CustomUser.objects.all()
@@ -16,4 +26,7 @@ class CreateUserView(CreateAPIView):
 
   def perform_create(self, serializer):
     serializer.save()
-    return Response({"message": "User created successfully", "user": serializer.data}, status=status.HTTP_201_CREATED)
+    return Response({"message": "User created successfully", "user": serializer.data}, status=status.HTTP_201_CREATED)\
+
+class LoginView(CustomObtainAuthToken):
+  pass
